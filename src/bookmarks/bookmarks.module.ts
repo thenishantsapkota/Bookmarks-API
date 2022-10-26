76b4f24181394a76f9322807a9f7c5f3b1eaddd0
Nishant Sapkota <snishant306@gmail.com>
2022-10-26 18:19:20 +0545

    feaat: add .env.example

--- src/bookmarks/bookmarks.module.ts
+++ src/bookmarks/bookmarks.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
-import { JwtStrategy } from 'src/auth/strategy';
 import { BookmarksController } from './bookmarks.controller';
 import { BookmarksService } from './bookmarks.service';
 
 @Module({
-  imports: [JwtModule.register({})],
   controllers: [BookmarksController],
-  providers: [BookmarksService, JwtStrategy],
+  providers: [BookmarksService],
 })
 export class BookmarksModule {}
